460419c215f7835fe5f6e756c76b64b4c3c2855c
Meri2233 <sharmamerika2@gmail.com>
2022-11-11 18:16:38 +0545

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  SearchBox,
+  PoweredBy,
+  Hits,
+  Highlight,
+} from 'react-instantsearch-hooks-web';
+
+
+const searchClient = algoliasearch(
+  "CKEFNWXMEN",
+  "3151a2d235d3b34f32e709eaafcd1a3c"
+);
+
+
+function Hit ({ hit }) {
+  return (
+  <div>
+    <img src={hit.avatar} alt={hit.username} />
+    <h1> <Highlight attribute="username" hit={hit} /> </h1>
+    <p><strong>Email:</strong> {hit.email}</p>
+    <p><strong>Bio:</strong>  {hit.bio}</p>   
+  </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <InstantSearch searchClient={searchClient} indexName='newindex'>
+
+      <h3 className='title'>Algolia Firebase Search</h3>
+
+      <SearchBox autoFocus placeholder='Search...' className='searchbox'/>
+
+      <PoweredBy className='poweredby'/>
+
+      <Hits hitComponent={Hit} className="hits"/>
+
+      </InstantSearch>
     </div>
   );
 }
